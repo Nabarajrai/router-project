c8d66ab5e70fca4417b696e030d27c1004cf89fb
nabaraj <nabaraj2055@gmail.com>
2021-05-26 00:18:38 +0545

    responsivve design

--- src/DogList.js
+++ src/DogList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 import "./doglist.css";
 
@@ -11,7 +12,11 @@ const DogList = ({ dogs }) => {
           {dogs.map((d, i) => (
             <Col key={i} className=" dog " md={6} lg={3}>
               <img src={d.src} alt={d.name} />
-              <h5>{d.name}</h5>
+              <h5>
+                <Link className="underline" to={`/dogs/${d.name}`}>
+                  {d.name}
+                </Link>
+              </h5>
             </Col>
           ))}
         </Row>
